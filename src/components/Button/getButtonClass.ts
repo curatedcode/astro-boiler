a6bb6570085c067b3702c0e33360a0d230b14b33
Zackary Fotheringham <me@zackaryf.com>
2025-06-07 18:38:39 -0400

    feat: add radix-ui color palette

--- src/components/Button/getButtonClass.ts
+++ src/components/Button/getButtonClass.ts
@@ -13,12 +13,12 @@ export function getButtonClass({
     "inline-flex h-10 px-4 py-2 items-center justify-center gap-2 whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition focus-visible:outline-1 focus-visible:outline-black focus-visible:outline-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0";
 
   const solidClass =
-    "bg-accent-9 text-accent-contrast hover:bg-accent-10 active:bg-accent-10 active:saturate-solid-active active:brightness-solid-active";
+    "bg-primary-9 text-primary-contrast hover:bg-primary-10 active:bg-primary-10 active:saturate-solid-active active:brightness-solid-active";
 
   const outlineClass =
-    "ring ring-accent-a8 text-accent-a11 hover:bg-accent-a2 active:bg-accent-a3";
+    "ring ring-primary-a8 text-primary-a11 hover:bg-primary-a2 active:bg-primary-a3";
 
-  const ghostClass = "hover:bg-accent-a3 active:bg-accent-a4";
+  const ghostClass = "hover:bg-primary-a3 active:bg-primary-a4";
 
   const iconSizeClass = "size-10";
 
